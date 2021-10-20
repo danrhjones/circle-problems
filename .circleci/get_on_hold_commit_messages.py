fd680feb3e4e34cd1e3970cf6b8c5d83ecc1ea15
Dan Jones <dan.rh.jones@gmail.com>
2021-10-20 12:06:49 +0100

    remove classes and functions

--- .circleci/get_on_hold_commit_messages.py
+++ .circleci/get_on_hold_commit_messages.py
@@ -33,107 +33,100 @@ class PipelineError(Exception):
 logger.info("Retrieving our own workflow information")
 
 
-class Workflow:
-    @classmethod
-    def get_workflows(self):
-        req = urllib.request.Request(
-            url='https://circleci.com/api/v2/workflow/{}'.format(
-                os.getenv('CIRCLE_WORKFLOW_ID')
-            ),
-            method='GET',
-            headers={
-                'Circle-Token': os.getenv('circle_token')
-            }
-        )
-        try:
-            with urllib.request.urlopen(req) as response:
-                response_raw_body = response.read()
-                parsed_response = json.loads(response_raw_body)
-                assert parsed_response.get('name'), \
-                    '‘{}’ has no ‘name’ key which is impossible'.format(parsed_response)
-                our_workflow = parsed_response
-        except (HTTPError, URLError) as error:
-            logging.error('Data not retrieved because %s\nURL: %s', error, req)
-            raise
+req = urllib.request.Request(
+    url='https://circleci.com/api/v2/workflow/{}'.format(
+        os.getenv('CIRCLE_WORKFLOW_ID')
+    ),
+    method='GET',
+    headers={
+        'Circle-Token': os.getenv('circle_token')
+    }
+)
+try:
+    with urllib.request.urlopen(req) as response:
+        response_raw_body = response.read()
+        parsed_response = json.loads(response_raw_body)
+        assert parsed_response.get('name'), \
+            '‘{}’ has no ‘name’ key which is impossible'.format(parsed_response)
+        our_workflow = parsed_response
+except (HTTPError, URLError) as error:
+    logging.error('Data not retrieved because %s\nURL: %s', error, req)
+    raise
 
-        project_pipelines_url = (
-            "https://circleci.com/api/v2/project/github/{}/{}/pipeline?{}"
-        ).format(
-            os.getenv('CIRCLE_PROJECT_USERNAME'),
-            os.getenv("CIRCLE_PROJECT_REPONAME"),
-            urllib.parse.urlencode({
-                'branch': os.getenv("CIRCLE_BRANCH"),
-            })
-        )
-        req = urllib.request.Request(
-            url=project_pipelines_url,
-            method='GET',
-            headers={
-                'Circle-Token': os.getenv('circle_token')
-            }
-        )
-        try:
-            with urllib.request.urlopen(req) as response:
-                response_raw_body = response.read()
-                parsed_response = json.loads(response_raw_body)
-        except PipelineError:
-            logger.exception("Exception while retrieving recent builds from CircleCI")
-            raise
+project_pipelines_url = (
+    "https://circleci.com/api/v2/project/github/{}/{}/pipeline?{}"
+).format(
+    os.getenv('CIRCLE_PROJECT_USERNAME'),
+    os.getenv("CIRCLE_PROJECT_REPONAME"),
+    urllib.parse.urlencode({
+        'branch': os.getenv("CIRCLE_BRANCH"),
+    })
+)
+req = urllib.request.Request(
+    url=project_pipelines_url,
+    method='GET',
+    headers={
+        'Circle-Token': os.getenv('circle_token')
+    }
+)
+try:
+    with urllib.request.urlopen(req) as response:
+        response_raw_body = response.read()
+        parsed_response = json.loads(response_raw_body)
+except PipelineError:
+    logger.exception("Exception while retrieving recent builds from CircleCI")
+    raise
 
-        pipelines = parsed_response['items']
+pipelines = parsed_response['items']
 
-        assert 0 < len(pipelines), \
-            'Circle thinks there are no pipelines which is impossible'
+assert 0 < len(pipelines), \
+    'Circle thinks there are no pipelines which is impossible'
 
-        logger.info('Found %d pipelines', len(pipelines))
+logger.info('Found %d pipelines', len(pipelines))
 
-        workflows = []
-        for pipeline in pipelines:
-            pipeline_workflow_url = "https://circleci.com/api/v2/pipeline/{}/workflow" \
-                .format(pipeline['id'])
-            req = urllib.request.Request(
-                url=pipeline_workflow_url,
-                method='GET',
-                headers={
-                    'Circle-Token': os.getenv('circle_token'),
-                    'Accept': 'application/json'
-                }
+workflows = []
+for pipeline in pipelines:
+    pipeline_workflow_url = "https://circleci.com/api/v2/pipeline/{}/workflow" \
+        .format(pipeline['id'])
+    req = urllib.request.Request(
+        url=pipeline_workflow_url,
+        method='GET',
+        headers={
+            'Circle-Token': os.getenv('circle_token'),
+            'Accept': 'application/json'
+        }
+    )
+    try:
+        with urllib.request.urlopen(req) as response:
+            response_raw_body = response.read()
+            parsed_response = json.loads(response_raw_body)
+    except BaseException:
+        logger.exception(
+            "Exception while retrieving recent builds from CircleCI")
+        raise
+
+    for workflow in parsed_response['items']:
+        assert workflow.get('name'), \
+            '‘{}’ has no ‘name’ key which is impossible'.format(
+                workflow
             )
-            try:
-                with urllib.request.urlopen(req) as response:
-                    response_raw_body = response.read()
-                    parsed_response = json.loads(response_raw_body)
-            except BaseException:
-                logger.exception(
-                    "Exception while retrieving recent builds from CircleCI")
-                raise
+        if workflow['name'] == our_workflow['name']:
+            if workflow['status'] == 'on_hold' or workflow['id'] == os.getenv('CIRCLE_WORKFLOW_ID'):
+                workflow['pipeline'] = pipeline
+                workflows.append(workflow)
 
-            for workflow in parsed_response['items']:
-                assert workflow.get('name'), \
-                    '‘{}’ has no ‘name’ key which is impossible'.format(
-                        workflow
-                    )
-                if workflow['name'] == our_workflow['name']:
-                    if workflow['status'] == 'on_hold':
-                        workflow['pipeline'] = pipeline
-                        workflows.append(workflow)
-                    if workflow['id'] == os.getenv('CIRCLE_WORKFLOW_ID'):
-                        workflows.append(workflow)
+workflows.sort(key=operator.itemgetter('created_at'))
 
-        workflows.sort(key=operator.itemgetter('created_at'))
+assert 0 < len(workflows), \
+    'CircleCI thinks there are no workflows which is impossible'
 
-        assert 0 < len(workflows), \
-            'CircleCI thinks there are no workflows which is impossible'
-
-        for workflow in workflows:
-            assert workflow.get('id'), \
-                '‘{}’ has no ‘id’ key which is impossible'.format(workflow)
-
-        return json.loads(json.dumps(workflows))
-
+for workflow in workflows:
+    assert workflow.get('id'), \
+        '‘{}’ has no ‘id’ key which is impossible'.format(workflow)
 
 commits = []
-for i in Workflow.get_workflows():
+
+for i in json.loads(json.dumps(workflows)):
     commits.append(i['pipeline']['vcs']['revision'])
 
 commitMessages = ""
